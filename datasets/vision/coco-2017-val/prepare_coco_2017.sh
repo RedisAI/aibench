5d685050473942a3f1bad643fa32ee31b3aaf073
filipe oliveira <filipecosta.90@gmail.com>
2022-12-05 21:48:37 +0000

    Fixed ck dataset fetching (#98)
    * Fixed ck dataset fetching. go mod tidy as well just to keep it pruned
    
    * Reverted go mod changes

--- datasets/vision/coco-2017-val/prepare_coco_2017.sh
+++ datasets/vision/coco-2017-val/prepare_coco_2017.sh
@@ -6,4 +6,4 @@ python3 -m pip install -r requirements.txt
 ck pull repo:ck-env
 ck install package --tags=object-detection,dataset,coco,2017,val,original
 ck locate env --tags=object-detection,dataset,coco,2017,val,original
-python3 preprocess.py --input-val_dir $(ck locate env --tags=object-detection,dataset,coco,2017,val,original)/val2017
+python3 preprocess.py --input-val_dir $(ck locate env --tags=object-detection,dataset,coco,2017,val,original | tail -1)/val2017
